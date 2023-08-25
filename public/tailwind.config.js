9b26e62c863879412c742fc8d3f98d37b0fd52c1
Tatiana_K <ottertut@gmail.com>
2023-08-25 18:38:36 +0300

    remove about

---
+++ public/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../content/*.md", "../layouts/**/*.html"],
+  theme: {
+    extend: {
+          gridTemplateColumns: {
+        '5': '10% 20% 40% 20% 10%'
+      }
+
+    },
+  },
+  plugins: [],
+}
+
