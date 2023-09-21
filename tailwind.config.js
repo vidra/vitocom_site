d5b7929f22bb7e5e336cc08ef3c81bfe7a3a914a
vidra <ottertut@gmail.com>
2023-09-21 19:19:32 +0000

    Updated site

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
           gridTemplateColumns: {
         '5': '10% 20% 40% 20% 10%'
-      }
+      },
+       backgroundImage: {
+         'hero-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('/src/img_foot.png')",
+      },
 
     },
   },
